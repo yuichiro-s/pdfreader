e02f47821019b080233ca663023258663c2badf4
Hiroyuki Shindo <hshindo@outlook.com>
2017-03-03 23:13:11 +0900

    Update pdfreader

--- src/DrawExtractor3.java
+++
@@ -1,9 +0,0 @@
-import org.apache.pdfbox.rendering.PageDrawer;
-import java.io.IOException;
-
-public class DrawExtractor3 extends PageDrawer {
-
-    public void process() throws IOException {
-
-    }
-}
