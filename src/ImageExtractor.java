6a04325e3a0b9c3cf3efb8319d68d0b8f441e8e6
Hiroyuki Shindo <hshindo@outlook.com>
2017-04-21 07:26:14 +0900

    Update ImageExtractor

--- src/ImageExtractor.java
+++ src/ImageExtractor.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.io.*;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.pdfbox.cos.COSBase;
@@ -46,14 +47,29 @@ public class ImageExtractor extends PDFStreamEngine {
     static void processFile(Path path) throws IOException {
         PDDocument doc = PDDocument.load(path.toFile());
         String outPath = path.toString().replace(".pdf", ".image");
-        ImageExtractor ie = new ImageExtractor();
-        ie.output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outPath), "UTF-8"));
-        ie.process(doc);
-        ie.output.close();
+
+        Writer w = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outPath), "UTF-8"));
+        for (int i = 0; i < doc.getNumberOfPages(); i++) {
+            ImageExtractor ie = new ImageExtractor();
+            ie.processPage(doc.getPage(i));
+            ie.filter();
+            ie.writeAll(w, i+1);
+        }
+        w.close();
     }
 
-    int pageNo;
-    Writer output;
+    class Position {
+        float x, y, w, h;
+
+        Position(float x, float y, float w, float h) {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.h = h;
+        }
+    }
+
+    List<Position> positions = new ArrayList<>();
 
     public ImageExtractor() throws IOException {
         addOperator(new Concatenate());
@@ -64,32 +80,82 @@ public class ImageExtractor extends PDFStreamEngine {
         addOperator(new SetMatrix());
     }
 
-    public void process(PDDocument doc) throws IOException {
-        pageNo = 1;
-        for (PDPage page : doc.getPages()) {
-            processPage(page);
-            pageNo++;
+    void writeAll(Writer w, int pageNum) throws IOException {
+        for (Position p : positions) {
+            List<String> l = new ArrayList<>();
+            l.add(String.valueOf(pageNum));
+            l.add(String.valueOf(p.x));
+            l.add(String.valueOf(p.y));
+            l.add(String.valueOf(p.w));
+            l.add(String.valueOf(p.h));
+            w.write(String.join("\t", l));
+            w.write("\n");
         }
+    }
+
+    void filter() throws IOException {
+        // filter adjacent images
+        List<Position> temp = new ArrayList<>();
+        for (Position p1 : positions) {
+            boolean isAdjacent = false;
+            int i = 0;
+            for (Position p2 : positions) {
+                boolean isSameX = p1.x == p2.x;
+                boolean isSameY = p1.y == p2.y;
+
+                float threshold = 2;
+                boolean near_bottom = 0 < p1.y - (p2.y + p2.h) && p1.y - (p2.y + p2.h) < threshold;
+                boolean near_top = 0 < (p1.y + p1.h) - p2.y && (p1.y + p1.h) - p2.y < threshold;
+                boolean near_right = 0 < (p1.x + p1.w) - p2.x && (p1.x + p1.w) - p2.x < threshold;
+                boolean near_left = 0 < p1.x - (p2.x + p2.w) && p1.x - (p2.x + p2.w) < threshold;
+                isAdjacent = (i > 0) && ((isSameX && (near_bottom || near_top)) || (isSameY && (near_left || near_right)));
+                i++;
+            }
+            if (!isAdjacent) temp.add(p1);
+        }
+        positions = temp;
+
+        // filter small image
+        temp = new ArrayList<>();
+        for (Position p : positions) {
+            if (!(p.w < 50 || p.h < 50)) temp.add(p);
+        }
+        positions = temp;
+
+        // filter same coordinate
+        temp = new ArrayList<>();
+        for (int i = 0; i < positions.size(); i++) {
+            Position p1 = positions.get(i);
+            boolean isSame = false;
+            for (int j = 0; j < positions.size(); j++) {
+                Position p2 = positions.get(j);
+                if (i != j && p1.x == p2.x && p1.y == p2.y && p1.w == p2.w && p1.h == p2.h) {
+                    isSame = true;
+                }
+            }
+            if (!isSame) temp.add(p1);
+        }
+        positions = temp;
     }
 
     @Override
     protected void processOperator(Operator operator, List<COSBase> operands) throws IOException {
         String operation = operator.getName();
-        if("Do".equals(operation)) {
+        if ("Do".equals(operation)) {
             COSName objectName = (COSName)operands.get(0);
             PDXObject xobject = getResources().getXObject(objectName);
 
             if (xobject instanceof PDImageXObject) {
                 PDImageXObject image = (PDImageXObject)xobject;
-                output.write(String.valueOf(pageNo)); output.write("\t");
 
-                // position (x, y, width, height)
                 Matrix ctmNew = getGraphicsState().getCurrentTransformationMatrix();
-                output.write(ctmNew.getTranslateX() + "\t" + ctmNew.getTranslateY() + "\t");
-                output.write(ctmNew.getScalingFactorX() + "\t" + ctmNew.getScalingFactorY());
-                output.write("\n");
+                float x = ctmNew.getTranslateX();
+                float y = ctmNew.getTranslateY();
+                float w = ctmNew.getScalingFactorX();
+                float h = ctmNew.getScalingFactorY();
+                positions.add(new Position(x, y, w, h));
             }
-            else if(xobject instanceof PDFormXObject) {
+            else if (xobject instanceof PDFormXObject) {
                 PDFormXObject form = (PDFormXObject)xobject;
                 showForm(form);
             }
