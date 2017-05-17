166a9fe129cb2fac6569e37eaf0221e712a90c92
Hiroyuki Shindo <hshindo@outlook.com>
2017-05-17 19:36:51 +0900

    Delete outpath

--- src/TextDrawExtractor.java
+++ src/TextDrawExtractor.java
@@ -41,7 +41,7 @@ public class TextDrawExtractor extends PDFGraphicsStreamEngine {
 
     static void processFile(Path path) throws IOException {
         PDDocument doc = PDDocument.load(path.toFile());
-        String outPath = path.toString() + ".txt";
+        //String outPath = path.toString() + ".txt";
 
         try (Writer w = new BufferedWriter(new OutputStreamWriter(System.out, "UTF-8"))) {
         //try (Writer w = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outPath), "UTF-8"))) {
